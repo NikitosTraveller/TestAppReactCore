3138b30a0fdbb73153d705971a88790afeaf9d49
nikita.semchenko <nikitos.traveler74@gmail.com>
2024-06-06 12:01:56 +0300

    init commit

---
+++ ReactApp1.Server/Data/AppDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TestApp.Server.Models;
+
+namespace TestApp.Server.Data
+{
+    public class AppDBContext : IdentityDbContext<User>
+    {
+
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+           Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ ReactApp1.Server/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TestApp.Server.Models
+{
+    public class User : IdentityUser
+    {
+        public int LoginCount { get; set; } = 0;
+
+        public string Name { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime LastLoginDate { get; set; } = DateTime.Now;
+
+        public bool IsAdmin { get; set; } = false;
+    }
+}
