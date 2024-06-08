ba951298b26ac122c8b3499dc583e0a279b42c25
nikita.semchenko <nikitos.traveler74@gmail.com>
2024-06-08 21:49:59 +0300

    migrations

---
+++ ReactApp1.Server/Migrations/20240608184919_Avatar.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ReactApp1.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class Avatar : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAdmin",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastLoginDate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Avatar",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "09262a80-844f-498e-b70b-6f9e4c917a16", null, "Admin", "ADMIN" },
+                    { "593fbc2e-1b4a-403c-b4a9-10b03890e894", null, "Regular", "REGULAR" },
+                    { "d1938b97-dd48-45b9-b664-bc5399392efd", null, "SuperAdmin", "SUPERADMIN" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "Avatar", "ConcurrencyStamp", "Email", "EmailConfirmed", "LastLoginDate", "LockoutEnabled", "LockoutEnd", "LoginCount", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "0374a4b0-e7d1-414b-a5c6-8cb41984a4de", 0, null, "865df87f-4088-47c6-9747-5152b2c1e812", "superadmin@gmail.com", false, null, false, null, 0, null, null, "AQAAAAIAAYagAAAAEKFQzYlkvn92dPRjL/6uO2Bi2YA8p7rRgluNM8cnQtne3LawKrcV0OMGskNJ3BrKZQ==", null, false, "3af4a227-7547-4162-9fec-2ca07b412041", false, "SuperAdmin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "d1938b97-dd48-45b9-b664-bc5399392efd", "0374a4b0-e7d1-414b-a5c6-8cb41984a4de" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "09262a80-844f-498e-b70b-6f9e4c917a16");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "593fbc2e-1b4a-403c-b4a9-10b03890e894");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "d1938b97-dd48-45b9-b664-bc5399392efd", "0374a4b0-e7d1-414b-a5c6-8cb41984a4de" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d1938b97-dd48-45b9-b664-bc5399392efd");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "0374a4b0-e7d1-414b-a5c6-8cb41984a4de");
+
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "LastLoginDate",
+                table: "AspNetUsers",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAdmin",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
